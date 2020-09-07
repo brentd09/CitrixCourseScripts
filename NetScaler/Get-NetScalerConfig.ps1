4f89c2c756b16504dd16a63b5a023e18fee5b979
brent denny <brent.denny@ddls.com.au>
2020-09-08 00:01:16 +1000

    fixed select issue

--- NetScaler/Get-NetScalerConfig.ps1
+++ NetScaler/Get-NetScalerConfig.ps1
@@ -3,8 +3,8 @@ Param (
   [PSCredential]$creds =  (Get-Credential -Message NSlogin -UserName nsroot),
   [Parameter(mandatory=$true)]
   [ValidateSet('interface','lbvserver','service','server')]
-  [string]$NetScalerElement
+  [string]$NetScalerObject
 )
 
-$res = Invoke-RestMethod -Method Get -Uri "http://192.168.10.101/nitro/v1/config/$NetScalerElement"   -Credential $creds
-$res.lbvserver | select *
+$res = Invoke-RestMethod -Method Get -Uri "http://192.168.10.101/nitro/v1/config/$NetScalerObject"   -Credential $creds
+$res | Select-Object -ExpandProperty $NetScalerObject
