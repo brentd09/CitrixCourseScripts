65353c6946555131e906ee2f6664ab92fbdcacc0
brent denny <brent.denny@ddls.com.au>
2021-04-13 11:14:59 +1000

    messing with OData API Citrix

---
+++ ODataAccess/Get-CtxODatav2.ps1
@@ -0,0 +1,52 @@
+[CmdletBinding()]
+Param (
+  [pscredential]$CtxCreds = (Get-Credential -Message 'Enter management credentials for Citrix' -Username 'workspacelab\administrator'),
+  [string]$DeliveryController = 'localhost'
+)
+function Select-Date {
+  Add-Type -AssemblyName System.Windows.Forms
+  Add-Type -AssemblyName System.Drawing
+  
+  $form = New-Object Windows.Forms.Form -Property @{
+    StartPosition = [Windows.Forms.FormStartPosition]::CenterScreen
+    Size          = New-Object Drawing.Size 243, 230
+    Text          = 'SearchDate'
+    Topmost       = $true
+  }
+  
+  $calendar = New-Object Windows.Forms.MonthCalendar -Property @{
+    ShowTodayCircle   = $false
+    MaxSelectionCount = 1
+  }
+  $form.Controls.Add($calendar)
+  
+  $okButton = New-Object Windows.Forms.Button -Property @{
+    Location     = New-Object Drawing.Point 38, 165
+    Size         = New-Object Drawing.Size 75, 23
+    Text         = 'OK'
+    DialogResult = [Windows.Forms.DialogResult]::OK
+  }
+  $form.AcceptButton = $okButton
+  $form.Controls.Add($okButton)
+  
+  $cancelButton = New-Object Windows.Forms.Button -Property @{
+    Location     = New-Object Drawing.Point 113, 165
+    Size         = New-Object Drawing.Size 75, 23
+    Text         = 'Cancel'
+    DialogResult = [Windows.Forms.DialogResult]::Cancel
+  }
+  $form.CancelButton = $cancelButton
+  $form.Controls.Add($cancelButton)
+  
+  $result = $form.ShowDialog()
+  
+  if ($result -eq [Windows.Forms.DialogResult]::OK) {$calendar.SelectionStart}
+}
+$SearchDate = Select-Date
+  if ($SearchDate -is [datetime]) {
+  $StringDate = Get-date -Date $SearchDate -UFormat "%Y-%m-%d"
+  $OdataUsers = Invoke-RestMethod -uri "http://localhost/Citrix/Monitor/Odata/v4/Data/Users" -Credential $CtxCreds
+  $Users = $OdataUsers.Value
+  $OdataSessions = Invoke-RestMethod -uri "http://localhost/Citrix/Monitor/Odata/v4/Data/Sessions" -Credential $CtxCreds
+  $Sessions = $OdataSessions.Value
+}
