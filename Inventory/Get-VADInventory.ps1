d3ecf4a4d7d262af3ed9806265134a584cf98722
brent denny <brent.denny@ddls.com.au>
2021-04-19 18:12:13 +1000

    more inv data in report

--- Inventory/Get-VADInventory.ps1
+++ Inventory/Get-VADInventory.ps1
@@ -1,5 +1,7 @@
 [Cmdletbinding()]
-Param ()
+Param (
+  [string]$DeliveryController = 'localhost'
+)
 Add-PSSnapin Citrix*
 function Get-CSS {
   $CSSContent = @'
@@ -19,13 +21,31 @@ th {padding-top: 12px;padding-bottom: 12px;text-align: left;background-color: #4
 }
 
 function Get-CTXSite {
-  Param ([string]$DeliveryController)
-  $CTXSite = Get-BrokerSite -AdminAddress $DeliveryController
+  Param ([string]$DDC)
+  $CTXSite = Get-BrokerSite -AdminAddress $DDC
   $CTXSiteHTML = $CTXSite | Select-Object -Property Name,LicenseServerName,LicensedSessionsActive,LicenseModel | 
-    ConvertTo-Html -Fragment -PreContent '<h2>Site Information</h2>' -As List 
+    ConvertTo-Html -Fragment -PreContent '<h2>Site Information</h2>' -As List | Out-String
   $CTXSiteHTML -replace '<table>','<table Class=ListTable>' -replace '<tr><td>','<tr><th>' -replace '</td><td>','</th><td>'
 }
 
+function Get-CtxMachCat {
+  Param ([string]$DDC)
+  $CTXCat = Get-BrokerCatalog -AdminAddress $DDC
+  $CTXCatHtml = $CTXCat | Select-Object -Property Name,AllocationType,SessionSupport,ZoneName | 
+    ConvertTo-Html -Fragment -PreContent '<br><h2>Machine Catalogs</h2>' | Out-String
+  $CTXCatHtml
+}
+
+function Get-CtxDelGrp {
+  Param ([string]$DDC)
+  $CTXDelGrp = Get-BrokerDesktopGroup -AdminAddress $DDC
+  $CTXDelGrpHtml = $CTXDelGrp | Select-Object -Property Name,DeliveryType,DesktopKind,Enabled,InMaintenanceMode | 
+    ConvertTo-Html -Fragment -PreContent '<br><h2>Delivery Groups</h2>' | Out-String
+  $CTXDelGrpHtml
+}
+
+
+# ----------------Main Code ----------------
 try {
   if (-not (Test-Path -PathType Container -Path C:\inetpub\wwwroot\reports)) {
     New-Item -Path C:\inetpub\wwwroot -Name Reports -ItemType Directory -ErrorAction Stop
@@ -33,6 +53,10 @@ try {
 }
 catch {write-warning "could not create the reports path under the IIS web directory"}
 
+$SiteFrag = Get-CTXSite -DDC $DeliveryController
+$MachCatFrag = Get-CtxMachCat -DDC $DeliveryController 
+$DelGrpFrag = Get-CtxDelGrp -DDC $DeliveryController
 
-$WebPage = ConvertTo-Html -Head (Get-CSS) -Body (Get-CTXSite -DeliveryController localhost)
-$WebPage | Out-File C:\inetpub\wwwroot\reports\index.html -Force
+
+$WebPage = ConvertTo-Html -Head (Get-CSS) -Body $SiteFrag,$MachCatFrag,$DelGrpFrag
+$WebPage | Out-File C:\inetpub\wwwroot\reports\index.html -Force 
