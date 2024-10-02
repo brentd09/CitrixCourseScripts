9f1e646e85ca1a1a2e3bd2da1525b39755efc204
Brent Denny <brent.denny@lumifygroup.com>
2024-10-02 13:32:22 +1000

    fixed the setting script

--- NetScaler/get-NSNitroInfo.ps1
+++ NetScaler/get-NSNitroInfo.ps1
@@ -12,12 +12,15 @@ function Convert-NitroApiWebTable {
     [string]$URL = 'https://developer-docs.netscaler.com/en-us/adc-nitro-api/current-release/configuration/lb/lbvserver#operations'
   )
   $ExtractedWebInfo = Get-NitroApiInfo -URL $URL
+
   $StartOfTable = $ExtractedWebInfo.Content.IndexOf('<table')
   $IndexOfTableEnd   = $ExtractedWebInfo.Content.IndexOf('</table>')
   $EndOfTable = $IndexOfTableEnd + 8
   $LengthOfTable = $EndOfTable - $StartOfTable
   $HtmlTableData = $ExtractedWebInfo.Content.Substring($StartOfTable,$LengthOfTable)
-  $CompactTable = $HtmlTableData -split "`n" -join ''
+  $SplitData = $HtmlTableData -split "`n"
+  $CompactTable = $SplitData -join ''
+  $NetscalerFeature = ($ExtractedWebInfo.Content -split "`n" | Where-Object {$_ -match '\<title\>'}) -replace '.*\<title\>([a-z0-9_]+).*','$1' 
   $HeaderEnd = $CompactTable.IndexOf('</thead>') + 8
   $Headers = $CompactTable.Substring(0,$HeaderEnd)
   $Body = $CompactTable -replace "$Headers",''
@@ -29,7 +32,10 @@ function Convert-NitroApiWebTable {
   }
   $CsvHeaders = ($Headers -replace '<.*?>',',' -replace ',+',',').trim(',') -split ',' -replace '\s+',''
   $TableToObject = $CsvBody | ConvertFrom-Csv -Delimiter '^' -Header $CsvHeaders
-  $NitroApiObject = $TableToObject | Out-GridView -Title 'Select which elements will be included in the configuration' -OutputMode Multiple
+  $NitroApiConfig = $TableToObject | Out-GridView -Title 'Select which elements will be included in the configuration' -OutputMode Multiple 
+  $NitroApiObject = [PSCustomObject]@{
+    $NetscalerFeature = $NitroApiConfig
+  }
   return $NitroApiObject
 }
 
@@ -38,17 +44,23 @@ function ConvertTo-NetscalerNitroJson {
   param (
     $NitroApiObject
   )
-  $ApiHashtable = [ordered]@{}
-  foreach ($Object in $NitroApiObject) {
-    $Value = Read-Host -Prompt "`nValue for $($Object.Name) `n$($Object.Description)"
+  $PropertyName = ($NitroApiObject | Get-Member -MemberType Properties).Name
+  $ApiConfig = [ordered]@{}
+  foreach ($Object in $NitroApiObject.$PropertyName) {
+    Write-Host -ForegroundColor Cyan "`n`nDesciption:`n$($Object.Description)"
+    Write-Host -ForegroundColor Yellow -NoNewline "`nType a Value for `"$($Object.Name)`": "
+    $Value = Read-Host 
     switch ($Object.DataType) {
       integer { [int]$TypeCastValue = $Value  }
       string  { [string]$TypeCastValue = $Value  }
       Default {}
     }
-    $ApiHashtable.Add($Object.Name,$TypeCastValue)
+    $ApiConfig.Add($Object.Name,$TypeCastValue)
   }
-  $ApiJson = $ApiHashtable | ConvertTo-Json -Depth 9
+  $APIComplete = @{
+    $PropertyName = $ApiConfig
+  }
+  $ApiJson = $APIComplete | ConvertTo-Json -Depth 9
   return $ApiJson
 }
 
